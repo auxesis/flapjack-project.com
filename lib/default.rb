551b5328774952221c11da84d8d99becfe010b2b
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-07-04 23:09:43 +0200

    print out error message if the file doesn't exist

--- lib/default.rb
+++ lib/default.rb
@@ -5,6 +5,11 @@ include Nanoc::Helpers::LinkTo
 
 def generate_docs_from_source(name)
   doc_path = File.join(@config[:flapjack_src], 'doc')
-  output = `rdiscount #{File.join(doc_path, "#{name.upcase}.md")}`
-  Haml::Helpers::find_and_preserve(output, ["pre"])
+  filename = File.join(doc_path, "#{name.upcase}.md")
+  if File.exists?(filename)
+    output = `rdiscount #{filename}`
+    Haml::Helpers::find_and_preserve(output, ["pre"])
+  else
+    "<span style='color: red'>Error: #{name.upcase}.md doesn't exist in #{doc_path}</span>"
+  end
 end
