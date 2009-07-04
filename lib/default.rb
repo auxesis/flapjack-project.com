670b4c3af300d641a000bc63fad557e1396ed1cf
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-07-04 17:57:54 +0200

    generate path to documentation from flapjack_src parameter in config

--- lib/default.rb
+++ lib/default.rb
@@ -4,6 +4,7 @@
 include Nanoc::Helpers::LinkTo
 
 def generate_docs_from_source(name)
-  output = `rdiscount #{File.join(File.dirname(__FILE__), '..', 'flapjack', 'doc', "#{name.upcase}.md")}`
+  doc_path = File.join(@config[:flapjack_src], 'doc')
+  output = `rdiscount #{File.join(doc_path, "#{name.upcase}.md")}`
   Haml::Helpers::find_and_preserve(output, ["pre"])
 end
